f4b0fcb645bf6757c78f8013b0db8e97d60fbc0e
nicoladomingorizzo <nicoladomingorizzo@gmail.com>
2025-06-09 22:12:36 +0200

    Change input fields to number type and update error message handling

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,16 +14,17 @@ const listNumbersEl = document.getElementById('listNumbers');
 const titleGameEl = document.getElementById('titleNumbers');
 const timerEl = document.getElementById('timer');
 const form = document.getElementById('formNumbers');
-const numberInput1 = document.getElementById('number-1');
-const numberInput2 = document.getElementById('number-2');
-const numberInput3 = document.getElementById('number-3');
-const numberInput4 = document.getElementById('number-4');
-const numberInput5 = document.getElementById('number-5');
-let errorMessageInput = document.getElementById('errorMessage');
+const numberInput1 = document.getElementById('number1Input');
+const numberInput2 = document.getElementById('number2Input');
+const numberInput3 = document.getElementById('number3Input');
+const numberInput4 = document.getElementById('number4Input');
+const numberInput5 = document.getElementById('number5Input');
+let messageInput = document.getElementById('message');
 const totNumbers = 5;
 const numbers = [];
+console.log(numbers)
 //Variabili input
-const numbersInput = [];
+let numbersInput = [];
 
 //Visualizzare in pagina 5 numeri casuali.
 function genRandonNum() {
@@ -45,7 +46,7 @@ for (let i = 0; i < numbers.length; i++) {
 
 //Da lì parte un timer di 30 secondi.
 let timerInterval;
-let timeLeft = 29;
+let timeLeft = 1;
 
 function updateTimer() {
     timerEl.innerHTML = timeLeft;
@@ -63,28 +64,31 @@ function updateTimer() {
 
 timerInterval = setInterval(updateTimer, 1000);
 
-const number1 = numberInput1.value;
-const number2 = numberInput2.value;
-const number3 = numberInput3.value;
-const number4 = numberInput4.value;
-const number5 = numberInput5.value;
-//inserisco i valori dei numeri inseriti nel form nell'array
-numbersInput.push(number1, number2, number3, number4, number5);
+
 /* Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile. */
 
 form.addEventListener('submit', function (e) {
+    const number1 = parseInt(numberInput1.value);
+    const number2 = parseInt(numberInput2.value);
+    const number3 = parseInt(numberInput3.value);
+    const number4 = parseInt(numberInput4.value);
+    const number5 = parseInt(numberInput5.value);
+    //inserisco i valori dei numeri inseriti nel form nell'array
+    numbersInput.push(number1, number2, number3, number4, number5);
+    console.log(numbersInput);
     e.preventDefault();
-    let thisNumberInput = 0;
-    for (let a = 0; a < numbers.length; a++) {
-        if (thisNumberInput == numbers[a]) {
-            thisNumberInput = thisNumberInput + 1;
+    let matchedNumbersCount = 0;
+    for (let a = 0; a < numbersInput.length; a++) {
+        if (numbers.includes(numbersInput[a])) {
+            matchedNumbersCount++;
+            console.log(matchedNumbersCount)
         }
     }
-    if (thisNumberInput == numbers.length) {
-        errorMessageInput.innerText = `hai indovinato tutti i numeri`;
+    if (matchedNumbersCount == numbers.length) {
+        messageInput.innerText = `Hai indovinato tutti i numeri`;
     } else {
-        errorMessageInput.innerText = `hai indovinato ${thisNumberInput} numeri, riprova a indovinarli tutti.
+        messageInput.innerText = `Hai indovinato ${matchedNumbersCount} numeri, riprova a indovinarli tutti.
         I numeri da indovinare erano : ${numbers}, quelli che hai inserito ${numbersInput}`;
     }
 }
